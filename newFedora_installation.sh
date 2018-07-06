c49a79177836b6fb696a75546ee2a71b31193ab0
nikotrone <marconicotra@vandebron.nl>
2018-07-06 11:46:01 +0200

    Shellcheck fixes

--- newFedora_installation.sh
+++ newFedora_installation.sh
@@ -4,16 +4,16 @@
 # TODO: check that the password is provided
 #if[ $0 ]
 
-echo -e "Welcome to your new Fedora environment!\n"
+echo -e "Welcome to your new Fedora environment!\\n"
 
 # Installing basic tools
-echo -e "Upgrading the system\t\t"
+echo -e "Upgrading the system\\t\\t"
 echo "$0" | sudo -S dnf -y update
 
 # TODO: check if last command returned a good exit value then echo OK, otherwise fail
-echo -e "[  OK  ]\n"
+echo -e "[  OK  ]\\n"
 
 
-echo -e "Installing git\t\t"
+echo -e "Installing git\\t\\t"
 echo "$0" | sudo -S dnf install git
-echo -e "[  OK  ]\n"
+echo -e "[  OK  ]\\n"
