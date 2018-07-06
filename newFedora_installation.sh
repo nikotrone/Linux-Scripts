b2c108a30bfa07a7e315a10b11cbb611be7ca410
nikotrone <marconicotra@vandebron.nl>
2018-07-06 13:37:49 +0200

    Fixed shellcheck

--- newFedora_installation.sh
+++ newFedora_installation.sh
@@ -2,18 +2,40 @@
 # In order to run the script needs the sudo password as the first argument in the command line
 
 # TODO: check that the password is provided
-#if[ $0 ]
+#if[ $? ]
 
 echo -e "Welcome to your new Fedora environment!\\n"
 
-# Installing basic tools
+# Upgrading system
 echo -e "Upgrading the system\\t\\t"
-echo -e "$0" | sudo -S dnf -y update
+# echo -e "$0" | sudo -S dnf -y update | cat > dnf_update_log
+if [[ $("echo -e $0 | sudo -S dnf -y update | cat > dnf_update_log") ]]; then
+    echo -e "[  OK  ]\\n"
+else
+    echo -e "[ FAIL ]\\n"
+fi
 
-# TODO: check if last command returned a good exit value then echo OK, otherwise fail
-echo -e "[  OK  ]\\n"
 
-
+# Installing basic tools
 echo -e "Installing git\\t\\t"
-echo "$0" | sudo -S dnf install git
-echo -e "[  OK  ]\\n"
+# echo "$0" | sudo -S dnf install git nano | cat >> dnf_install_basic_log
+if [[ $("echo $0 | sudo -S dnf install git nano | cat >> dnf_install_basic_log") ]]; then
+    echo -e "[  OK  ]\\n"
+else
+    echo -e "[ FAIL ]\\n"
+fi
+
+
+echo -e "Checking if ll shortcut is in place\\t\\t"
+if [[ $("ll > temp.log") ]]; then
+    echo -e "[  OK  ]\\n"
+else
+    echo -e "\\n ll shortcut does not exist, creating shortcut now\\n" >> dnf_install_basic_log
+    if [ -e ~/.bash_profile ]
+    then
+        echo "alias ll='ls -lGh \$@'" >> ~/.bash_profile
+    else
+        echo "alias ll='ls -lGh \$@'" > ~/.bash_profile
+    fi
+    echo -e "[ DONE ]\\n"
+fi
