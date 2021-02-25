f007880b95a32f959646fcc5a81415bca0e6af92
nikotrone <nikotrone@gmail.com>
2021-02-25 19:37:02 +0100

    disable addigy

---
+++ disable_Addigy.sh
@@ -0,0 +1,6 @@
+sudo rm -f /Library/CS/falcond
+sudo rm -r /Library/LaunchAgents/screenconnect-*
+sudo rm -f /Library/LaunchAgents/com.teamviewer*
+sudo rm -f /Library/LaunchAgents/connectwisecontrol-*
+sudo rm -f /Library/LaunchAgents/com.addigy.*
+sudo rm -rf /Library/Addigy/
